7a65971e4d8b8cf507676ff071a98dbf039dd220
Karn Rahal <karn.rahal@alumni.ubc.ca>
2020-04-12 16:52:12 -0700

    Updates

---
+++ app-295b4136ef5fd81fad13.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[],[["UxWs",6,4,5,0]]]);
+//# sourceMappingURL=app-295b4136ef5fd81fad13.js.map
--- src/utils/typography.js
+++
@@ -1,7 +0,0 @@
-import Typography from "typography"
-import usWebDesignStandardsTheme from 'typography-theme-us-web-design-standards'
-
-const typography = new Typography(usWebDesignStandardsTheme)
-
-export const { scale, rhythm, options } = typography
-export default typography
