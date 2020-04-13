459b7c8216a2a9446076e30097977b28458ca91d
Karn Rahal <karn.rahal@alumni.ubc.ca>
2020-04-12 19:31:47 -0700

    updated package json files and chagned favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import favicon from '../static/favicon.ico'
 
 export default () => (
     <div className="container">
@@ -8,12 +9,13 @@ export default () => (
             <title>Karn Rahal</title>
             <meta name="description" content="Karn Rahal | Software Engineer" />
             <html lang="en" />
+            <link rel="icon" href={favicon} />
         </Helmet>
         <div>
             <div>
                 <h1>Hey, I'm Karn.</h1>
                 <p>
-                    I'm an ambitious software engineer experienced in web and mobile development, machine learning, and DevOps. My passion lies in building solutions to real, human problems.
+                    I'm an ambitious software engineer experienced in web and mobile development, machine learning, and DevOps.
                 </p>
                 <p>
                     Currently, I'm studying Computer Engineering at <a href="https://www.ubc.ca/" target="_blank" rel="noopener">UBC</a> and will be graduating this May.
